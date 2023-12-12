289276077ca21f106196ce06ba247a3411231f6b
Ayumi Otake <ayumi.otake@rakus-partner.co.jp>
2023-12-12 23:25:35 +0900

    11KM

--- src/node/index.js
+++ src/node/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
+const port = 13519;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-const port = 13519;
 
 const cors = require("cors");
 app.use(cors());
@@ -31,18 +31,26 @@ app.get("/customers", async (req, res) => {
   }
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.post("/add-customer", async (req, res) => {
   try {
     const { company_name, industry, contact, location } = req.body;
-    console.log("リクエストを受け取りました:", req.body); //大竹12/7追加
+    console.log("リクエストを受け取りました:", req.body);
     const newCustomer = await pool.query(
       "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [company_name, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false });
+  }
+});
+
+app.delete("/delete-customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const deleteCustomer = await pool.query("DELETE FROM customers WHERE customer_id = $1", [customerId]);
+    res.json({ success: true });
   } catch (err) {
     console.error(err);
     res.json({ success: false });
